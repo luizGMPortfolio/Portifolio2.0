e16648ad59c322d5233d78715fd4fa8c97c3371a
Luiz Gustavo Da Silva Moraes <luizgm.dev@gmail.com>
2024-07-02 11:52:02 -0300

    Update Midias.jsx

--- App/src/components/Midias.jsx
+++ App/src/components/Midias.jsx
@@ -1,10 +1,11 @@
 import './Midias.css'
 
+// eslint-disable-next-line react/prop-types
 const Midias = ({Type}) => {
   return (
     <div className='midias'>
-        <div className={`git ${Type === 'white' ? 'GitWhite':'GitBlack'}`}></div>
-        <div className={`link ${Type === 'white' ? 'Linkwhite':'LinkBlack'}`}></div>
+        <a target="_blank" href='https://github.com/luizGMPortfolio' className={`git ${Type === 'white' ? 'GitWhite':'GitBlack'}`}></a>
+        <a target="_blank" href='https://www.linkedin.com/in/luiz-gustavo-da-silva-moraes-9276b72a0/' className={`link ${Type === 'white' ? 'Linkwhite':'LinkBlack'}`}></a>
     </div>
   )
 }
